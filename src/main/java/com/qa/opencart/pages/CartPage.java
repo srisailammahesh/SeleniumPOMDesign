88f3bb25ad45a025c14605bccd6008641a1f9787
srisailammahesh <srisailammahesh@gmail.com>
2025-09-09 17:22:56 +0530

    done changes as per review comments in My Pr

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,8 +1,15 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
-String cart = "cart";
+
+By Cart = By.id("cart");
  public void addToCart() {
+	 System.out.println("Adding to CartPage");
+	 System.out.println("Adding to CartPage");
+	 System.out.println("Adding to CartPage");
+	 System.out.println("Adding to CartPage");
 	 System.out.println("Adding to CartPage");
  }
 }
