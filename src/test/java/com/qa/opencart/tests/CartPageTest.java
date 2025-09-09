7ed7937049d15aa41b4da533eca774dc91493349
srisailammahesh <srisailammahesh@gmail.com>
2025-09-09 15:39:47 +0530

    Cart Page test Cases

---
+++ src/test/java/com/qa/opencart/tests/CartPageTest.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.tests;
+
+import org.testng.annotations.Test;
+
+public class CartPageTest {
+	@Test
+	public void cartTest() {
+		System.out.println("Cart Testing");
+	}
+
+}
